c3571d2b3d362ae90bb8a228471edf0848d8ccf1
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-08-30 13:20:17 -0500

    Add YAML

--- suffragist.rb
+++ suffragist.rb
@@ -1,19 +1,38 @@
 require 'sinatra'
+require 'yaml/store'
 
 get '/' do
   @title = 'Welcome to the Suffragist!'
   erb :index
 end
 
+# post '/cast' do
+#   @title = 'Thanks for casting your vote!'
+#   @vote  = params['vote']
+#   erb :cast
+# end
+#
+# get '/results' do
+#   @title = 'Results'
+#   @votes = { 'HAM' => 7, 'PIZ' => 5, 'CUR' => 3 }
+#   erb :results
+# end
 post '/cast' do
   @title = 'Thanks for casting your vote!'
   @vote  = params['vote']
+  @store = YAML::Store.new 'votes.yml'
+  @store.transaction do
+    @store['votes'] ||= {}
+    @store['votes'][@vote] ||= 0
+    @store['votes'][@vote] += 1
+  end
   erb :cast
 end
 
 get '/results' do
-  @title = 'Results'
-  @votes = { 'HAM' => 7, 'PIZ' => 5, 'CUR' => 3 }
+  @title = 'Results so far:'
+  @store = YAML::Store.new 'votes.yml'
+  @votes = @store.transaction { @store['votes'] }
   erb :results
 end
 
